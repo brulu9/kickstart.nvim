a30e56e59050135b2fee0f7e12855d1ce72d9909
brulu <brunnerlukas@outlook.de>
2025-11-23 18:16:51 +0100

    removed lsp-latex via treesitter

--- init.lua
+++ init.lua
@@ -686,27 +686,28 @@ require('lazy').setup({
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
+        --    https://github.latexindentcom/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
+
         texlab = {
           settings = {
-            texlab = {
-              diagnostics = {
-                -- Filter out unwanted diagnostics/warnings for LaTeX
-                ignoredPatterns = {
-                  'LaTeX hooks Warning',
-                  'Underfull',
-                  'Overfull',
-                  'LaTeX Warning: .+ float specifier changed to',
-                  'Package siunitx Warning: Detected the "physics" package:',
-                  'Package hyperref Warning: Token not allowed in a PDF string',
-                  'Fatal error occurred, no output PDF file produced!',
-                },
+            diagnostics = {
+              ignoredPatterns = {
+                'LaTeX hooks Warning',
+                'Underfull',
+                'Overfull',
+                'LaTeX Warning: .+ float specifier changed to',
+                'Package siunitx Warning: Detected the "physics" package:',
+                'Package hyperref Warning: Token not allowed in a PDF string',
+                'Fatal error occurred, no output PDF file produced!',
               },
             },
+            bibtexFormatter = 'texlab',
+            latexFormatter = 'latexindent',
+            formatterLineLength = 80,
           },
         },
 
@@ -799,6 +800,7 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        latex = { 'latexindent' },
       },
     },
   },
@@ -861,7 +863,7 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        preset = 'enter',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
@@ -975,10 +977,11 @@ require('lazy').setup({
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'latex' },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        additional_vim_regex_highlighting = { 'ruby', 'latex' },
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
